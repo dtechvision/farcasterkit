ba77f7b1288597cd7d1e20394dce951bdecf6910
Samuel <samu@hubi.net>
2024-03-12 22:31:40 +0100

    make non opinionated and future proof for versions/endpoints

--- src/providers/thirdparty/neynarProvider.ts
+++ src/providers/thirdparty/neynarProvider.ts
@@ -10,13 +10,13 @@ export class NeynarProvider extends ThirdParty {
     /**
      * NeynarProvider for getting Data with neynar.com Farcaster APIs
      * @param apiKey Neynar API Key to use. Please do not hardcode this! Use .env or other
-     * @param endPoint optional Neynar API endpoint, defaults to public Neynar V2. Custom endpoint is required to be Neynar API V2 compliant! 
+     * @param endPoint optional Neynar API endpoint, defaults to public Neynar https://api.neynar.com
      */
     constructor(apiKey: string, endPoint?: string) {
         if (endPoint) {
             super("neynar", apiKey, endPoint);
         } else {
-            super("neynar", apiKey, "https://api.neynar.com/v2/farcaster");
+            super("neynar", apiKey, "https://api.neynar.com");
         }
     }
 }
